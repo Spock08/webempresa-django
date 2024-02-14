329d580a3d4903bdccc09eefebd1a4a92d09987b
Emilio Ojeda <emiliojedal@gmail.com>
2024-02-13 23:42:25 -0300

    feat: add core app, and frontend with html templates

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    #Admin Path URLs
     path('admin/', admin.site.urls),
+    #Core Path URLs
+    path('', include('core.urls'))
 ]
